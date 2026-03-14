db5fabe9a954b57ca379f76d3e914c72ab738951
Oiva <oiva.paatsema@gmail.com>
2026-03-14 22:27:21 +0200

    Update 3.py

--- Tehtävät/3.py
+++ Tehtävät/3.py
@@ -1,7 +1,6 @@
 import mysql.connector
 from geopy import distance
 
-
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port= 3306,
@@ -17,7 +16,6 @@ def hae_kentan_tiedot(icao):
     sql = "SELECT name, latitude_deg, longitude_deg FROM airport WHERE ident = %s"
     kursori = yhteys.cursor()
 
-
     kursori.execute(sql, (icao,))
     tulos = kursori.fetchone()
 
@@ -28,15 +26,13 @@ def hae_kentan_tiedot(icao):
             "matka": (tulos[1], tulos[2])
         }
 
-
-# kysyy köyttäjältä ensimäisen ja toisen kentän icao koodin
+# kysyy käyttäjältä ensimäisen ja toisen kentän icao koodin
 icao1 = input("Anna ensimmäisen kentän ICAO-koodi: ").upper()
 icao2 = input("Anna toisen kentän ICAO-koodi: ").upper()
 
 # lisää kentät pää ohjelmaan
 kentta1 = hae_kentan_tiedot(icao1)
 kentta2= hae_kentan_tiedot(icao2)
-
 
 # katsoo jos annetuissa koodeissa on kenttö
 if kentta1 and kentta2:
@@ -48,5 +44,3 @@ else:
     print("Virheelinen koodi")
 # kutsuu ohjelman
 yhteys.close()
-
-
